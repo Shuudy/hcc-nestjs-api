0780a1ea9c82b979e3a8d5d05b32a76279a1dfdb
Shuudy <ancieux.jerome059@gmail.com>
2024-11-24 15:23:30 +0100

    feat(auth): add role-based access control in AuthGuard
    - Integrated `Reflector` to retrieve required roles via `@Roles` decorator
    - Enhanced `AuthGuard` to validate user roles against the JWT payload

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,11 +1,16 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { ROLES_KEY } from '../roles/roles.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly reflector: Reflector,
+  ) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
 
@@ -19,10 +24,20 @@ export class AuthGuard implements CanActivate {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: 'secret'
       });
+
       request['member'] = payload;
-    } catch {
+    } catch {      
       throw new UnauthorizedException();
     }
+
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ]);
+    if (requiredRoles && !requiredRoles.includes(request['member'].role)) {
+      throw new ForbiddenException();
+    }
+
     return true;
   }
 
---
+++ src/roles/role.enum.ts
@@ -0,0 +1,5 @@
+export enum RoleEnum {
+    CONTRIBUTOR = 'contributor',
+    PLAYER = 'player',
+    COACH = 'coach',
+}
