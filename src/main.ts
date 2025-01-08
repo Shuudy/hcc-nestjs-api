11c9dbf7206a28f5d02f3a8035e312a605672065
Shuudy <ancieux.jerome059@gmail.com>
2025-01-08 10:13:48 +0100

    feat(swagger): setup swagger module for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('HCC API')
+    .setDescription('A NestJS API for managing members, news, and events for a handball club with JWT-based authentication.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
